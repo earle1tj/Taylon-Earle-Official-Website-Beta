2437434355646236fb42559f3a493e50442ed4b1
Taylon Earle <earle.taylon@gmail.com>
2022-11-08 21:21:53 -0500

    Updated

---
+++ src/app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const nodemailer = require('nodemailer');
+require('dotenv').config()
+
+
+const path = require('path')
+
+const PORT = 3000;
+
+const app = express();
+
+
+let transporter = nodemailer.createTransport({
+    host: 'taylonearle.com',
+    port: 465,
+    secure: true,
+    auth: {       
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+
+    }
+});
+
+
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get('/', (req, res) => {
+    res.render("home", {
+
+    });
+
+});
+
+
+
+app.get('/contact', (req, res) => {
+    res.render("contact", {
+
+    });
+
+});
+
+
+app.post('/contact', (req, res) => {
+
+    let mailOptions = {
+        from: process.env.MAIL_USERNAME,
+        to: 'info@taylonearle.com',
+        subject: req.body.subject,
+        text: "From: " + req.body.email + "\nName: " + req.body.name + "\nMessage: " + req.body.message + "\n" + req.body.authorization,
+}
+
+
+
+    transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+            console.log("Error " + err);
+        } else {
+            console.log("Email sent successfully");
+        }
+    })
+
+    res.redirect("/");
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
+})
